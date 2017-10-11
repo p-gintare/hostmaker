5889201093890e9b7a2af374eb8635220509f98a
Gintare Pacauskaite <gintare.pacauskaite@adform.com>
2017-10-12 00:08:23 +0300

    what you can get test description

---
+++ Hostmaker.Selenium/Hostmaker.Selenium/Features/WhatYouCanGetCalculationTests.feature
@@ -0,0 +1,14 @@
+﻿Feature: CalculationFormTests
+	In order to be able to check my property value
+	As a property owner
+	I want to be able to select my property address and bedroom number and get estimate what I can make
+
+
+Scenario: I can get my property estimate
+	Given I go to Hostmaker.co
+	When I enter my postcode 'N1 9 PD'
+	And I select 'White Lion Street' as my address
+	And I select 2 bedroom from the number of bedrooms option
+	And I enter email address as 'test@test.com'
+	And I click calculate
+	Then I should see the estimate we can make between 1075 and 1200
---
+++ Hostmaker.Selenium/Hostmaker.Selenium/Steps/WhatYouCanGetCalculationTestsSteps.cs
@@ -0,0 +1,50 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Hostmaker.Selenium.Steps
+{
+    [Binding]
+    public class WhatYouCanGetCalculationTestsSteps
+    {
+        [Given(@"I go to Hostmaker\.co")]
+        public void GivenIGoToHostmaker_Co()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter my postcode '(.*)'")]
+        public void WhenIEnterMyPostcode(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I select '(.*)' as my address")]
+        public void WhenISelectAsMyAddress(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I select (.*) bedroom from the number of bedrooms option")]
+        public void WhenISelectBedroomFromTheNumberOfBedroomsOption(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter email address as '(.*)'")]
+        public void WhenIEnterEmailAddressAs(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click calculate")]
+        public void WhenIClickCalculate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see the estimate we can make between (.*) and (.*)")]
+        public void ThenIShouldSeeTheEstimateWeCanMakeBetweenAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
